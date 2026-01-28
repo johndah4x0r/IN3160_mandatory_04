bfc5fd6b2d6865edd0160c3ff66c4c4dde9fbc4d
John Isaac Calderon <ogclapdrop@gmail.com>
2026-01-28 15:56:24 +0100

    implemented 8-bit shifter + test bench
    An 8-bit shifter has been implemented in accordance with the
    description in task 4a. It behaves somewhat adversely, in that
    the reset flag doesn't apply for the first clock cycle: if
    `rst_n` isn't held for at least two clock cycles, the output
    will remain undefined for the next 8 cycles.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

---
+++ src/shift8.vhd
@@ -0,0 +1,46 @@
+/*
+    Implementation of an 8-bit shift register
+*/
+
+-- Load libraries
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- define 8-bit shift register module
+entity shift8 is
+    generic (
+        s_width: positive := 8
+    );
+
+    port (
+        serial_in: in std_ulogic; -- serial input
+        rst_n, mclk: in std_ulogic; -- reset and clock
+
+        b: out std_ulogic_vector(s_width - 1 downto 0); -- parallel output
+        serial_out: out std_ulogic -- serial output
+    );
+end entity shift8;
+
+architecture rtl of shift8 is
+    component shift_reg is
+        generic (
+            width: positive := s_width
+        );
+
+        port (
+            serial_in: in std_ulogic;
+            rst_n, mclk: in std_ulogic;
+            b: out std_ulogic_vector(s_width - 1 downto 0);
+            serial_out: out std_ulogic
+        );
+    end component;
+begin
+    sr: shift_reg
+        port map (
+            serial_in => serial_in,
+            rst_n => rst_n,
+            mclk => mclk,
+            b => b,
+            serial_out => serial_out
+        );
+end architecture rtl;
