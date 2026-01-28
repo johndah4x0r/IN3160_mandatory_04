0e19862c7b30d93e1f39e5e9a614b7fd3488b5a7
John Isaac Calderon <ogclapdrop@gmail.com>
2026-01-28 16:14:26 +0100

    implemented 32-bit shifter + test bench
    A 32-bit shifter has been implemented in accordance with the
    description in task 4b. An appropriate test bench will
    accompany the component.
    
    This is more or less a pointless exercise, as this is basically
    task 4c in disguise: we are using the asset `shift_reg` in all
    of the tasks 4a, 4b, 4c.
    
    Signed-off-by: John Isaac Calderon <ogclapdrop@gmail.com>

---
+++ test/tb_shift32.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import Timer, ValueChange
+
+# Compensate for 1e3 multiplicative error
+TIME_UNIT="us"
+
+@cocotb.test()
+async def main_test(dut):
+    dut._log.info("Applying stimuli...")
+    
+    # DUT in reset
+    dut.rst_n.value = 0
+
+    # Default serial input
+    dut.serial_in.value = 0
+    
+    # Starting clock
+    dut._log.info("Starting clock")
+    cocotb.start_soon(Clock(dut.mclk, 100, unit=TIME_UNIT).start())
+    
+    # Hold the reset flag for 200 ns, then
+    # unset it (rst_n <= 1), then apply a
+    # 300 ns pulse at the serial input
+    # - the reset command doesn't apply
+    # for the first clock cycle
+    await Timer(200, unit=TIME_UNIT)
+    dut.rst_n.value = 1
+    dut.serial_in.value = 1
+
+    await Timer(300, unit=TIME_UNIT)
+    dut.serial_in.value = 0
+
+    # apply a reset commant 500 ns later
+    await Timer(500, unit=TIME_UNIT)
+    dut.rst_n.value = 0
+    await Timer(100, unit=TIME_UNIT)
+    dut.rst_n.value = 1
+
+    await Timer(200, unit=TIME_UNIT)
+    dut._log.info("Stimuli done")
